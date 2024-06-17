d0e91db01dee942930cf137ba818852f0454af9a
Golisciano <83238594+Golisciano@users.noreply.github.com>
2024-06-16 23:00:55 -0300

    Add files via upload

---
+++ desktop/bin/main/mapaniveluno/piedra.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="piedra" tilewidth="34" tileheight="34" tilecount="256" columns="16">
+ <image source="piedra.png" width="564" height="563"/>
+</tileset>
